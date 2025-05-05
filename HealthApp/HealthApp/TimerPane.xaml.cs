da6d5bf21850d44fe0d506bd8cea827f929215b3
Daniel Green <daniel.green927@outlook.com>
2025-05-04 22:58:14 -0700

    timer devt

--- HealthApp/HealthApp/TimerPane.xaml.cs
+++ HealthApp/HealthApp/TimerPane.xaml.cs
@@ -3,7 +3,11 @@
  * CS361, Spring 2025
  */
 
+
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace HealthApp
 {
@@ -12,9 +16,49 @@ namespace HealthApp
     /// </summary>
     public partial class TimerPane : UserControl
     {
+        private readonly Dispatcher _dispatcher;
+        public static Stopwatch paneTimer = new();
+
         public TimerPane()
         {
             InitializeComponent();
+            _dispatcher = Application.Current.Dispatcher;
+            Task.Factory.StartNew(() =>
+            {
+                UpdateTimerText(this,_dispatcher);
+            });
+        }
+
+        private static async Task UpdateTimerText(TimerPane tp, Dispatcher dp)
+        {
+            DateTime lastUpdate = DateTime.MinValue;
+
+            while (true)
+            {
+                // only update once a second
+                if (paneTimer.IsRunning && (paneTimer.Elapsed.TotalMilliseconds - lastUpdate.Millisecond > 1000))
+                {
+                    if (!dp.HasShutdownStarted)
+                    {
+                        await Application.Current.Dispatcher.InvokeAsync(() =>
+                        {
+                            // update label with message we received
+                            tp.timerTextBlock.Text = new string($"{paneTimer.Elapsed.ToString(@"hh\:mm\:ss")}");
+                        });
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            paneTimer.Start();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            paneTimer.Stop();
         }
     }
 }
--- HealthApp/HealthApp/userMessageServer.py
+++ HealthApp/HealthApp/userMessageServer.py
@@ -8,23 +8,20 @@ context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://127.0.0.1:5555")
 
-while True:
-    message = socket.recv().decode()
-    print(f'Recieved client message: {message}')
-    if len(message) > 0:
-        if message == 'Q':
-            print('Client has requested to close connection!')
-            break
-        if message != 'MSG':
-            socket.send_string('ERR')
+message = socket.recv().decode()
+print(f'Recieved client message: {message}')
+if len(message) > 0:
+    if message == 'Q':
+        print('Client has requested to close connection!')
+    if message != 'MSG':
+        socket.send_string('ERR')
 
-        randInt = datetime.datetime.now()
-        randInt = str(((randInt.second) * (randInt.microsecond)) % 100)
-        with open('encouragement.json', 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            friendlyMessage = data[randInt]
-            file.close()
-        print(f'Sending response: {friendlyMessage}')
-        socket.send_string(friendlyMessage)
-
+    randInt = datetime.datetime.now()
+    randInt = str(((randInt.second) * (randInt.microsecond)) % 100)
+    with open('encouragement.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        friendlyMessage = data[randInt]
+        file.close()
+    print(f'Sending response: {friendlyMessage}')
+    socket.send_string(friendlyMessage)
 context.destroy()
