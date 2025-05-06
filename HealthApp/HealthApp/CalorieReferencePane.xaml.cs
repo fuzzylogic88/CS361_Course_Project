32ec282f7271de69d28e35051c4a8fd67894d050
Daniel Green <daniel.green927@outlook.com>
2025-05-05 19:13:11 -0700

    Begin scaffolding search/sort function for food pane

--- HealthApp/HealthApp/CalorieReferencePane.xaml.cs
+++ HealthApp/HealthApp/CalorieReferencePane.xaml.cs
@@ -13,10 +13,14 @@ namespace HealthApp
     /// </summary>
     public partial class CalorieReferencePane : UserControl
     {
+        public static List<string> FoodList = [];
+        public readonly string food_fname = "foodinfo.txt";
+
         public CalorieReferencePane()
         {
             InitializeComponent();
 
+            
             var foods = new List<FoodEntryControl>()
             {
                 new() {
@@ -31,6 +35,46 @@ namespace HealthApp
                 },
             };
             foodList.ItemsSource = foods;
+        }
+
+        public void GetFoodCategories()
+        {
+
+        }
+
+        public bool FoodExists(string name)
+        {
+            try
+            {
+                List<string> accountCollection = [.. File.ReadAllLines(account_fname)];
+                foreach (string account in accountCollection)
+                {
+                    string[] split = account.Split(',');
+                    if (split.Length == 2)
+                    {
+                        if (split[0] == user && pass == string.Empty)
+                        {
+                            return true;
+                        }
+                        else if (split[0] == user && !string.IsNullOrEmpty(pass))
+                        {
+                            if (split[1] == pass)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
